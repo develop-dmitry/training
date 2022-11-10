43b7f9b5ecffd7fb8b1ddc7264597341a2429f85
Dmitry Kozlov <d.kozlov@intensa.ru>
2022-11-11 00:10:15 +0300

    Fix mysql

--- html/index.php
+++ html/index.php
@@ -1,1 +1,9 @@
-<?php phpinfo() ?>
+<?php
+
+try {
+    $pdo = new PDO('mysql:dbname=money;host=mysql', 'root', '');
+
+    echo 'Соединение с базой успешно установлено';
+} catch (PDOException $exception) {
+    echo 'Произошла ошибка';
+}
